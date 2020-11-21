63234cc53d3e1796abdaf6745a52dd881b798668
jharpe7 <60944831+jharpe7@users.noreply.github.com>
2020-11-21 17:22:26 -0500

    Add files via upload

--- h1.sql
+++ h1.sql
@@ -1,81 +1,63 @@
 SET FOREIGN_KEY_CHECKS = 0;
-/* 
-Create table */
+
 CREATE TABLE IF NOT EXISTS EMPLOYEE
-(Ssn numeric(9) NOT NULL,
-Ename varchar(20),
-Address VARCHAR(40),
-Telephone numeric(10),
-PRIMARY KEY (Ssn));
-
-CREATE TABLE IF NOT EXISTS MANAGER
-(m_uname varchar(15) NOT NULL,
-is_admin boolean NOT NULL,
-shift VARCHAR(15),
-m_pword varchar(15) NOT NULL,
-Essn numeric(10) NOT NULL,
-PRIMARY KEY (m_uname),
-FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn));
-
-CREATE TABLE IF NOT EXISTS HOUSEKEEPER
-(shift VARCHAR(15),
-Essn numeric(10) NOT NULL,
-Super_Ssn numeric(10) NOT NULL,
-FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn),
-FOREIGN KEY (Super_Ssn) REFERENCES EMPLOYEE(Ssn));
-
-CREATE TABLE IF NOT EXISTS RECEPTIONIST
-(r_uname varchar(15) NOT NULL,
-shift VARCHAR(15),
-r_pword varchar(15) NOT NULL,
-Essn numeric(10) NOT NULL,
-Super_Ssn numeric(10) NOT NULL,
-PRIMARY KEY (r_uname),
-FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn),
-FOREIGN KEY (Super_Ssn) REFERENCES EMPLOYEE(Ssn));
+(fName varchar(20) NOT NULL,
+lName varchar(20) NOT NULL,
+ssn char(9) NOT NULL,
+address varchar(40),
+telephone numeric(10),
+e_username varchar(20) NOT NULL,
+e_password varchar(20) NOT NULL,
+e_type varchar(15) NOT NULL,
+shift int NOT NULL,
+super_ssn char(9),
+UNIQUE (e_username),
+PRIMARY KEY(ssn),
+FOREIGN KEY (super_ssn) REFERENCES EMPLOYEE(ssn));
 
 
 CREATE TABLE IF NOT EXISTS ROOM
-(r_num VARCHAR(4) NOT NULL,
-m_Ssn NUMERIC(9) NOT NULL,
-k_Ssn NUMERIC(9),
+(r_num numeric(3) NOT NULL,
+c_Ssn char(9) NOT NULL,
 r_description VARCHAR(90) NOT NULL,
-r_price FLOAT,
+r_price int,
 r_beds INT,
 r_capacity INT,
 r_name VARCHAR(100),
 r_floor  INT NOT NULL,
-is_available boolean,
+r_available boolean,
 PRIMARY KEY (r_num),
-FOREIGN KEY (m_Ssn) REFERENCES EMPLOYEE(Ssn),
-FOREIGN KEY (k_Ssn) REFERENCES EMPLOYEE(Ssn));
+FOREIGN KEY (c_Ssn) REFERENCES EMPLOYEE(ssn));
 
 CREATE TABLE IF NOT EXISTS RESERVATION
 (res_num NUMERIC(8) NOT NULL,
-r_Ssn NUMERIC(9) NOT NULL,
-checkin date,
-checkout date,
-r_num VARCHAR(4) NOT NULL,
-guest_uname VARCHAR(15) NOT NULL,
+r_username varchar(20) NOT NULL,
+checkin_date date,
+checkout_date date,
+r_num numeric(3) NOT NULL,
+guest_username VARCHAR(20) NOT NULL,
+checked_in boolean,
+checked_out boolean,
 PRIMARY KEY (res_num),
-FOREIGN KEY (r_Ssn) REFERENCES EMPLOYEE(Ssn),
-FOREIGN KEY (guest_uname) REFERENCES GUEST(uname));
+FOREIGN KEY (r_username) REFERENCES EMPLOYEE(e_username),
+FOREIGN KEY (guest_username) REFERENCES GUEST(g_username));
 
 CREATE TABLE IF NOT EXISTS BILL
 (res_num NUMERIC(8) NOT NULL,
 paid_on date,
 payment_amount FLOAT,
 cc_info NUMERIC(16),
-guest_uname VARCHAR(15) NOT NULL,
+guest_username VARCHAR(20) NOT NULL,
 FOREIGN KEY (res_num) REFERENCES RESERVATION(res_num),
-FOREIGN KEY (guest_uname) REFERENCES GUEST(uname));
+FOREIGN KEY (guest_username) REFERENCES GUEST(g_username));
 
 CREATE TABLE IF NOT EXISTS GUEST
-(uname VARCHAR(15) NOT NULL,
-g_name VARCHAR(15),
-cc_info NUMERIC(16),
-email VARCHAR(30) NOT NULL,
-phone NUMERIC(10),
-PRIMARY KEY (uname));
-
+(fName VARCHAR(20) NOT NULL,
+lName VARCHAR(20) NOT NULL,
+email VARCHAR(30),
+phone NUMERIC(10) NOT NULL,
+cc_info NUMERIC(16) NOT NULL,
+g_username VARCHAR(20) NOT NULL,
+g_password VARCHAR(20) NOT NULL,
+PRIMARY KEY (g_username));
 SET FOREIGN_KEY_CHECKS = 1;
